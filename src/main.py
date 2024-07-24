9ad1347cc9a2064905bafc77ad9e61430364591c
johannesrhvw <johannes.v.wittke@karevo.de>
2024-07-24 12:56:12 +0200

    mainwindow settings

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title("Hello World!")
+root.geometry("300x200")
+root.minsize(width=200, height=100)
+root.resizable(width=True, height=True)
 # parent, text
-tk.Label(root, text="Hello World!").pack()
+label = tk.Label(root, text="Hello World!")
+label.pack()
 root.mainloop()
